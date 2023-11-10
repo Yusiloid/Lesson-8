72201f8ccbb1793675d3c01e01ea314dc134f0eb
Жека Айтишник <yustus7798@gmail.com>
2023-11-10 14:28:25 +0300

    Коммит от 10.11.2023

---
+++ Lesson 7.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from flask import Flask
+
+text = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').text
+currencies = json.loads(text)
+
+
+for currency in currencies['Valute']:
+    print(currency)
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    text = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').text
+    currencies = json.loads(text)
+    result = ''
+    for currency in currencies['Valute']:
+        result += str(currency) + '<br>'
+    return result
+
+if __name__ == "__main__":
+    app.run()
